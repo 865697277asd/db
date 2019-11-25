869dafe34df6f839cee84bb77d80773ef502279e
wiser <2273341398@qq.com>
2019-11-25 13:49:07 +0800

    提交springboot-git

---
+++ src/test/java/com/exampl/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampl.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
